93dec5c1a41399db7650f4860910b11f45c5e59a
Alexcander-Jing <18061523086@163.com>
2024-08-26 11:18:18 +0800

    code of ablation study added, 2024.8.26

---
+++ Simulation_ExperimentsBashes/online_simulation_method4_EEGNet_noRest_fixedepoch_feadistillation_val_21_9batchsize_Rest_2_5_3_mixed_7_3_ablation4.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# 你的程序的路径
+program_path="Online_simulation/Online_train_EEGNet_simulation_method4_fixedepoch_FeatureDistillation_val_21_9batchsize_Rest_2_5_mixed_6_2_ablation1.py"
+
+# 你的工作目录的路径
+workspace_folder="/home/jyt/workspace/MI_Online_Adjusting"
+
+# 改变当前工作目录
+cd $workspace_folder
+
+# 循环运行你的程序
+for i in $(seq 1 25)
+do
+  sub_name=$(printf "%03d" $i)
+  python3 $program_path \
+    --seed 42 \
+    --gpu_idx 0 \
+    --sub_name $sub_name \
+    --Offline_folder_path "/home/jyt/workspace/transfer_models/datasets_MI/hand_elbow/derivatives" \
+    --windows_num 120 \
+    --trial_pre 120 \
+    --proportion 0.75 \
+    --Offline_result_save_rootdir "Offline_simulation_experiments/method2_EEGNet_val_classval_pretrainlight_unfreeze_new" \
+    --Online_folder_path "Online_DataCollected" \
+    --Online_result_save_rootdir "Online_simulation_experiments/method4_EEGNet_fixedepoch_FeatureDistillation_val_21_9batchsize_Rest_2_lessepoch_1_8_mixed_7_new_3_3_ablation4" \
+    --restore_file "pretrained_weights/checkpoints_test_predict/checkpoints_test_encoder3_light/encoder_epoch_1.0.pt" \
+    --n_epoch_offline 16 \
+    --n_epoch_online  2 \
+    --batch_size 16 \
+    --mode "online" \
+    --batch_size_online 9 \
+    --trial_nums 96 \
+    --best_validation_path "lr0.01_dropout0.5" \
+    --unfreeze_encoder_offline "True" \
+    --unfreeze_encoder_online "True" \
+    --alpha_distill 0.5 \
+    --update_trial 1 \
+    --update_wholeModel 12 \
+    --para_m 0.90 \
+    --cons_rate 1.0
+done
